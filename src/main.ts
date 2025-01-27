661d1fd97f4e813537fee03693ab418d2a83304d
Ashwin Sathian <ashwinsathyan19@gmail.com>
2025-01-27 12:01:51 +0530

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.enableCors({
+    origin: ['https://laminar-app.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Laminar API')
     .setDescription('The Laminar API description')
